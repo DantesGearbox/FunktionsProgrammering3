65af2622ac1b33029b39cfe36765038afe5a5452
saladoor <sghali@hotmail.com>
2016-10-25 13:53:10 +0200

    names

--- Assignment 3.fs
+++ Assignment 3.fs
@@ -1,3 +1,8 @@
-﻿type Outcome = | S | F
+﻿//          02157 - Functional Programming
+//            Assignment 3 - 27/10/2016
+//      Sami Ghali 144443    Mads Engberg 144460
+
+
+type Outcome = | S | F
 type Sample = Outcome list
 type ProbTree = | 
