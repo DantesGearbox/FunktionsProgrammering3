a75e2c25b17fa1bfc72a8e27a02ed2b64ee8735b
DantesGearbox <mads.engberg@hotmail.com>
2016-10-25 15:02:50 +0200

    Opgave 1 done

--- Assignment 3.fs
+++ Assignment 3.fs
@@ -6,3 +6,12 @@ type ProbTree = | Branch of string * float * ProbTree * ProbTree
 let exp = Branch(">2", 0.67, Branch(">3", 0.5, Leaf "A", Leaf "B")
                            , Branch(">3", 0.5, Leaf "C", Leaf "D" ))
 
+//Problem 4.1
+let rec probOK pt = 
+    match pt with
+    | Leaf s -> true
+    | Branch(_, prob, pt1, pt2) -> (0.0 < prob && prob < 1.0) && probOK pt1 && probOK pt2;;
+
+
+
+
