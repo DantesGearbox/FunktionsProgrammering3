573dd14d9d3a2480374a01ef27b157b77ab631c5
DantesGearbox <mads.engberg@hotmail.com>
2016-10-25 13:52:34 +0200

    Type declarations

--- Assignment 3.fs
+++ Assignment 3.fs
@@ -1,3 +1,8 @@
 ﻿type Outcome = | S | F
 type Sample = Outcome list
-type ProbTree = | 
+type ProbTree = | Branch of string * float * ProbTree * ProbTree
+                | Leaf of string
+
+let exp = Branch(">2", 0.67, Branch(">3", 0.5, Leaf "A", Leaf "B")
+                           , Branch(">3", 0.5, Leaf "C", Leaf "D" ))
+
